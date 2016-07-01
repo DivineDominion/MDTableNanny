05e0f2f68d2bf39c20e76ddc13c982b0ac6811c3
Christian Tietze <christian.tietze@gmail.com>
2016-07-01 08:55:12 +0200

    remove notion of "selecting" a table

--- MDTableNanny/MultipleTables.swift
+++ MDTableNanny/MultipleTables.swift
@@ -7,12 +7,8 @@ protocol MultipleTables {
 
     var tables: [TableState] { get }
     var containsTable: Bool { get }
-    var selectedTable: Int { get set }
-    @available(*, deprecated=1.0)
-    var currentTableState: TableState { get }
 
     mutating func replaceTable(index index: Int, table: TableState)
-    mutating func selectTable(index index: Int)
 
     func rowStream() -> FragmentedMarkdownRowStream
 }
@@ -20,12 +16,4 @@ protocol MultipleTables {
 extension MultipleTables {
 
     var containsTable: Bool { return !tables.isEmpty }
-    var currentTableState: TableState { return tables[selectedTable] }
-
-    mutating func selectTable(index index: Int) {
-
-        guard (0...tables.count).contains(index) else { return }
-
-        selectedTable = index
-    }
 }
